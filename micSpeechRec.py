dd2b89d4f35cbee8303f43edaedd195f9a8296a8
valentin bonassies <valentin.bonassies@gmail.com>
2020-01-10 14:09:24 +0100

    add error check for mic transcription

--- micSpeechRec.py
+++ micSpeechRec.py
@@ -1,9 +1,29 @@
 import speech_recognition as sr
 
 r = sr.Recognizer()
-print(sr.Microphone.list_microphone_names())
-mic = sr.Microphone(device_index=3)
+#print(sr.Microphone.list_microphone_names())
+mic = sr.Microphone()
+response = {
+    "success": True,
+    "error": None,
+    "transcription": None
+}
+
 with mic as source:
     r.adjust_for_ambient_noise(source)
     audio = r.listen(source)
-print(r.recognize_google(audio))
+
+try:
+    rec = r.recognize_google(audio, language="fr-FR")
+    response["transcription"] = rec
+    print(rec)
+
+except sr.RequestError:
+    # API was unreachable or unresponsive
+    response["success"] = False
+    response["error"] = "API unavailable"
+except sr.UnknownValueError:
+    # Speech was unintelligible
+    response["error"] = "Unable to recognize speech"
+
+print(response)
